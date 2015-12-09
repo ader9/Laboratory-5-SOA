bfb1d832df1aed54d3505e47fda32c84b283a812
Adrian <650220@unizar.es>
2015-12-09 19:02:53 +0100

    set max twits

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -9,10 +9,10 @@ public class Router extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("direct:search")
-			.to("twitter://search?consumerKey={{twitter.consumerKey}}&"
-					+ "consumerSecret={{twitter.consumerSecret}}&"
-					+ "accessToken={{twitter.accessToken}}&"
-					+ "accessTokenSecret={{twitter.accessTokenSecret}}");
+				.to("twitter://search?consumerKey={{twitter.consumerKey}}&"
+						+ "consumerSecret={{twitter.consumerSecret}}&"
+						+ "accessToken={{twitter.accessToken}}&"
+						+ "accessTokenSecret={{twitter.accessTokenSecret}}");
 	}
 
 }
--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
+
 
 	@Autowired
 	  private ProducerTemplate producerTemplate;
@@ -23,6 +27,15 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
-    }
+        HashMap<String, Object> header = new HashMap<String, Object>();
+        if(q.contains("max:")) {
+            String[] part = q.split(" max:");
+            header.put("CamelTwitterKeywords", part[0]);
+            header.put("CamelTwitterCount", part[1]);
+        }
+        else{
+            header.put("CamelTwitterKeywords", q);
+        }
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", header);
+}
 }
